8d964b23e87de99b0743ab3a321da5c3ea5b19c0
Fais Salaeh <faiss.slh@gmail.com>
2023-02-06 20:03:56 +0700

    Create Login/Register page
    using Laravel Ui and vue.js

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', 'AuthController@login');
+Route::get('register', 'AuthController@register');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
